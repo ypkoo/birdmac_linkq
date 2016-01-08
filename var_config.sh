d57d5788a93f2e08f3c1f4066a433783b699c36e
ypkoo <ypkoo93@gmail.com>
2016-01-08 16:15:38 +0100

    initial commit

---
+++ var_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source config.cfg
+
+period=$(expr $1 \* 3600)
+timeout=$(expr $period \* 30 \* 1000)
+
+echo "period="$period"
+sigma="$2"
+topology="$3"
+seed="$4"
+check_rate="$5"
+density="$6"
+mac="$7"
+lq="$8"
+home_dir=/home2/linkq/$7/lanada/app
+result_dir="$title"_"$7"
+rawdata_dir="$title"_"$7"/rawdata
+timeout="$timeout"" > var_config.cfg
